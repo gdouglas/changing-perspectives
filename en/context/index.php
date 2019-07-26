4cedfae8be32bd2b710a48180d1874b59069cb19
Graham <graham.douglas@ubc.ca>
2019-07-26 12:20:13 -0700

    blocked out slider

--- en/context/index.php
+++ en/context/index.php
@@ -23,7 +23,31 @@
         <h1 class="site-title">Context</h1>
         <div class="pull-quote text-center">
             <p>The world during the times of George Vancouver</p>
+            <ul class="slider">
+                <li class="slide"><a href="/en/context#"><img src="https://via.placeholder.com/450x250">Context</a></li>
+                <li class="slide"><a href="/en/context#mod-1"><img src="https://via.placeholder.com/450x250">Overview</a></li>
+                <li class="slide"><a href="/en/context#mod-2"><img src="https://via.placeholder.com/450x250">Pacific Northwest Coast before European contact</a></li>
+                <li class="slide"><a href="/en/context#mod-3"><img src="https://via.placeholder.com/450x250">Global Context: European empire building</a></li>
+                <li class="slide"><a href="/en/context#mod-4"><img src="https://via.placeholder.com/450x250">Contact & Trade in the PNWC prior to Vancouver</a></li>
+                <li class="slide"><a href="/en/context#mod-5"><img src="https://via.placeholder.com/450x250">Learning Activities</a></li>
+            </ul>
         </div>
+        <section id="mod-1" class="slide-content">
+            <h2>Module 1</h2>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore molestiae quibusdam magni animi consectetur, totam ab perspiciatis nostrum facilis ratione repudiandae esse natus numquam neque dolor delectus ducimus quas! Odit!</div>
+        </section>
+        <section id="mod-2" class="slide-content">
+            <div>hello 2</div>
+        </section>
+        <section id="mod-3" class="slide-content">
+            <div>hello 3</div>
+        </section>
+        <section id="mod-4" class="slide-content">
+            <div>hello 4</div>
+        </section>
+        <section id="mod-5" class="slide-content">
+            <div>hello 5</div>
+        </section>
     </main>
     <?php include $_SERVER['DOCUMENT_ROOT'] . "/en-footer.php" ?>
 </body>

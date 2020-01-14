ad0ab29a13689ec23c0d397db27a24fc76309c7e
Graham <graham.douglas@ubc.ca>
2020-01-14 10:25:39 -0800

    added slide for game

--- en/technologies/index.php
+++ en/technologies/index.php
@@ -12,13 +12,78 @@
         <?php include $_SERVER['DOCUMENT_ROOT'] . "/en-header.php" ?>
     </header>
     <main id="main" tabindex="-1">
-        <h1>Navigation and Ship Technology: Game</h1>
+        <h1>Navigation and Ship Technology</h1>
         <div class="pull-quote text-center">
-            <p>Learn about sailing a ship and take the wheel!</p>
+            <p>The world during the times of George Vancouver</p>
         </div>
-        <div id="game">
-            <button>Ready to play?</button>
+        <div class="slider-container relative">
+            <ul class="slider">
+                <li class="slide active">
+                    <div style="padding:56.25% 0 0 0;position:relative;"><iframe id="vimeo1" src="https://player.vimeo.com/video/356042135?color=ee314a&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div>
+                    <script src="https://player.vimeo.com/api/player.js"></script>
+                    <h3 class="gold">Navigation</h3>
+                    <img class="slide-icon" src="/images/icons/video-solid.svg" alt="video icon">
+                </li>
+                <li class="slide">
+                    <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/356042272?color=ee314a&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div>
+                    <script src="https://player.vimeo.com/api/player.js"></script>
+                    <h3 class="gold">Ships</h3>
+                    <img class="slide-icon" src="/images/icons/video-solid.svg" alt="video icon">
+                </li>
+                <li class="slide">
+                    <figure>
+                        <img src="/images/sextant.jpg" alt="brass sextant">
+                        <figcaption>
+                            <h3 class="gold">Sextant</h3>
+                            <p>Brass <strong>sextant</strong> with five eyepieces, made by Jesse Ramsden, was used by Joseph Baker, Vancouver’s First Lieutenant and is on loan courtesy of Richard Baker. The sextant is a doubly reflecting navigation instrument with a graduated arc of 60° and a sighting mechanism, used for measuring the angular distances between objects and especially for taking altitudes in navigation.</p>
+                        </figcaption>
+                    </figure>
+                    <img class="slide-icon" src="/images/icons/newspaper-regular.svg" alt="article icon">
+                </li>
+                <li class="slide">
+                    <figure>
+                        <img src="/images/telescope.jpg" alt="brass telescope">
+                        <figcaption>
+                            <h3 class="gold">Telescope</h3>
+                            <p>Two stage, leather-covered <strong>telescope</strong> made by G. or P. Dolland, inscribed "Capt. John. Baker Royal Navy" on loan courtesy of Richard Baker. Joseph Baker, Vancouver’s First Lieutenant . A telescope is an optical instrument which magnifies distant objects by arranging lenses or curved mirrors to collect and focus light.</p>
+                        </figcaption>
+                    </figure>
+                    <img class="slide-icon" src="/images/icons/newspaper-regular.svg" alt="article icon">
+                </li>
+                <li class="slide">
+                    <figure>
+                        <img src="/images/chronometer.jpg" alt="brass chronometer">
+                        <figcaption>
+                            <h3 class="gold">Arnold Chronometer</h3>
+                            <p>John Arnold (maker) <strong>"Arnold 176" chronometer</strong> used by George Vancouver for 1790 expedition into Pacific Northwest. Completed in 1791 for the Board of Longitude, now in gimbal suspension with 3 tier mahogany case, purchased from Christie’s Auction House.</p>
+                            <p>A marine Chronometer is a timepiece that is precise and accurate enough to be used as a portable time standard, meaning it can be used to determine longitude by accurately measuring the time of a known fixed location. When first developed in the 18th century, it was a major technical achievement, as accurate knowledge of the time during sea voyage is necessary for navigation without the use of electronic or communication devices.</p>
+                        </figcaption>
+                    </figure>
+                    <img class="slide-icon" src="/images/icons/newspaper-regular.svg" alt="article icon">
+                </li>
+                <li class="slide">
+                    <figure>
+                        <img src="/images/chronometer.jpg" alt="brass chronometer">
+                        <figcaption>
+                            <h3 class="gold">Sailing Game</h3>
+                            <!-- todo let link work -->
+                            <a href="./game">Plan Game</a>
+                        </figcaption>
+                    </figure>
+                    <img class="slide-icon" src="/images/icons/gamepad-solid.svg" alt="game icon">
+                </li>
+            </ul>
+            <div class="slider-controls">
+                <button id="prev" type="button" class="btn" disabled>Previous</button>
+                <button id="next" type="button" class="btn">Next</button>
+            </div>
         </div>
+        <section class="intructor-resources">
+            <h2>Instructor Resources</h2>
+            <a href="/files/technology/navigation-learning-activity.pdf" target="_blank" download class="download-link"><img src="/images/icons/file-download-solid.svg" alt="download icon" class="icon">Navigation learning activity</a>
+            <a href="/files/technology/navigation-learning-activity-guidelines-for-teachers.pdf" target="_blank" download class="download-link"><img src="/images/icons/file-download-solid.svg" alt="download icon" class="icon">Navigation Learning Activity Guidelines For Teachers</a>
+        </section>
+
     </main>
     <?php include $_SERVER['DOCUMENT_ROOT'] . "/en-footer.php" ?>
 </body>

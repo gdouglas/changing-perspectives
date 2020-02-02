bf049ee1cdeb5ae30396eabfaa4468eb0e412edc
Graham <graham.douglas@ubc.ca>
2020-02-02 12:06:22 -0800

    Gulp: enabled browsersync, php serv, css/php watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,61 @@
 const gulp = require('gulp');
+const { series } = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const rename = require("gulp-rename");
 const babel = require('gulp-babel');
-const { watch } = require('gulp');
+var php = require('gulp-connect-php');
+var browserSync = require('browser-sync');
+
+// concat and minify site js
 function scripts() {
     return gulp.src(["./scripts/validation.js", "./scripts/app.js"])
-      .pipe(concat('app.js'))
-      .pipe(gulp.dest('./dist'))
-      .pipe(babel({
-          presets: ['@babel/preset-env']
-      }))
-      .pipe(rename("app.min.js"))
-      .pipe(uglify())
-      .pipe(gulp.dest('./dist'))
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(babel({
+            presets: ['@babel/preset-env']
+        }))
+        .pipe(rename("app.min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'))
 };
 
-
-exports.watch = function(){
-    watch('scripts/**/*', scripts);
+// start php server
+function connectSync() {
+    php.server({
+        // a standalone PHP server that browserSync connects to via proxy
+        port: 8000,
+        keepalive: true,
+        base: "./"
+    }, function (){
+        browserSync({
+            proxy: '127.0.0.1:8000'
+        });
+    });
 }
-exports.default = scripts;
+
+// start broswersync
+function browserSync(done) {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        port: 8000
+    });
+    done();
+}
+
+// call this to reload the browser
+function browserSyncReload(done) {
+    browserSync.reload();
+    done();
+}
+// tasks run when files are changed
+function watchFiles() {
+    gulp.watch('scripts/**/*', gulp.series(scripts, browserSyncReload));
+    gulp.watch('**/*.css', browserSyncReload);
+    gulp.watch('**/*.php', browserSyncReload);
+}
+
+const watch = gulp.parallel([watchFiles, connectSync]);
+exports.default = watch;

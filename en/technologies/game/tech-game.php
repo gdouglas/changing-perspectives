6506d5e325d191dc9dc3a476b1139693535e66f1
Graham <graham@Grahams-MacBook-Pro.local>
2020-02-04 08:18:28 -0800

    game js ajax

--- en/technologies/game/tech-game.php
+++ en/technologies/game/tech-game.php
@@ -1,6 +1,15 @@
 <?php
 
-$r = $_REQUEST;
+$q = $_GET;
 
-var_dump($r);
+var_dump(array_key_last($q));
+$answers = [
+    "question1" => "a1",
+    "question2" => "b2",
+    "question3" => "c3",
+    "question4" => "b4",
+    "question5" => "a5",
+    "question6" => "c6"
+];
+
 ?>

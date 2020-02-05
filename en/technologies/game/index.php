6194735055288f046545e5d49415bee82c8d6c32
Graham <graham.douglas@ubc.ca>
2020-02-04 22:09:32 -0800

    Forms for questions now functional

--- en/technologies/game/index.php
+++ en/technologies/game/index.php
@@ -15,7 +15,6 @@
         </style>
     </noscript>
 </head>
-
 <body class="splash">
     <a class="skip-main" href="#main">Skip to main content</a>
     <header class="">
@@ -28,88 +27,7 @@
             <div id="game">
                 <p id="game-intro">Want to sail a ship? Take this quiz then take the wheel!</p>
                 <a href="#game" class="btn btn-action">Ready to play?</a>
-                <div id="play">
-                    <form>
-                        <fieldset>
-                            <legend>
-                                <h2>Learn The Language of Sailing</h2>
-                            </legend>
-                            <div class="question active">
-                                <h3>Question 1: Lorem Ipsum Dolor Sit Amet</h3>
-                                <div class="tile">
-                                    <label class="tile" for="a1">
-                                        <div>A</div>
-                                        <img src="http://placehold.jp/150x100.png">
-                                    </label>
-                                    <input type="radio" name="question1" id="a1" value="a1">
-                                </div>
-                                <div class="tile">
-                                    <label class="tile" for="b1">
-                                        <div>B</div>
-                                        <img src="http://placehold.jp/150x100.png">
-                                    </label>
-                                    <input type="radio" name="question1" id="b1" value="b1">
-                                </div>
-                                <div class="tile">
-                                    <label class="tile" for="c1">
-                                        <div>C</div>
-                                        <img src="http://placehold.jp/150x100.png">
-                                    </label>
-                                    <input type="radio" name="question1" id="c1" value="c1">
-                                </div>
-                            </div>
-                            <div class="question">
-                                <h3>Question 2: Lorem Ipsum Dolor Sit Amet</h3>
-                                <div class="tile">
-                                    <label class="tile" for="a2">
-                                        <div>A</div>
-                                        <img src="http://placehold.jp/150x100.png">
-                                    </label>
-                                    <input type="radio" name="question1" id="a2" value="a2">
-                                </div>
-                                <div class="tile">
-                                    <label class="tile" for="b2">
-                                        <div>B</div>
-                                        <img src="http://placehold.jp/150x100.png">
-                                    </label>
-                                    <input type="radio" name="question1" id="b2" value="b2">
-                                </div>
-                                <div class="tile">
-                                    <label class="tile" for="c2">
-                                        <div>C</div>
-                                        <img src="http://placehold.jp/150x100.png">
-                                    </label>
-                                    <input type="radio" name="question1" id="c2" value="c2">
-                                </div>
-                            </div>
-                            <div class="question">
-                                <h3>Question 3: Lorem Ipsum Dolor Sit Amet</h3>
-                                <div class="tile">
-                                    <label class="tile" for="a3">
-                                        <div>A</div>
-                                        <img src="http://placehold.jp/150x100.png">
-                                    </label>
-                                    <input type="radio" name="question1" id="a3" value="a3">
-                                </div>
-                                <div class="tile">
-                                    <label class="tile" for="b3">
-                                        <div>B</div>
-                                        <img src="http://placehold.jp/150x100.png">
-                                    </label>
-                                    <input type="radio" name="question1" id="b3" value="b3">
-                                </div>
-                                <div class="tile">
-                                    <label class="tile" for="c3">
-                                        <div>C</div>
-                                        <img src="http://placehold.jp/150x100.png">
-                                    </label>
-                                    <input type="radio" name="question1" id="c3" value="c3">
-                                </div>
-                            </div>
-                        </fieldset>
-                        <button type="button" onclick="nextQuestion()" class="needjs btn">Next Question</button>
-                    </form>
-                </div>
+                <?php include "./tech-game.php"?>
             </div>
         </div>
     </main>
--- en/technologies/game/tech-game.php
+++ en/technologies/game/tech-game.php
@@ -1,8 +1,17 @@
 <?php
+$questionNum = 1;
 
-$q = $_GET;
-
-var_dump(array_key_last($q));
+if (count($_POST) > 0) {
+    $q = array_keys($_POST)[0];
+    $questionNum = substr($q, -1)+1;
+    var_dump($questionNum);
+    if ($questionNum > 3 ) {
+        echo 'You\'ve learned the lingo now let\'s <a href=".\sail\">Set Sail!</a>';
+        return;
+    }
+    $a = $_POST[1];
+    echo "For $q you answered $a";
+}
 $answers = [
     "question1" => "a1",
     "question2" => "b2",
@@ -12,4 +21,84 @@ $answers = [
     "question6" => "c6"
 ];
 
-?>
+$questions = [
+    1 =>  array(
+        "image" => "http://placehold.jp/150x100.png",
+        "question" => "What do you call a fish with legs?",
+        "options" => array(
+        "a" => array(
+            "image" => "http://placehold.jp/150x100.png",
+            "option" => "fishy"
+        ),
+        "b" => array(
+            "image" => "http://placehold.jp/150x100.png",
+            "option" => "not fishy"
+        ),
+        "c" => array(
+            "image" => "http://placehold.jp/150x100.png",
+            "option" => "Rodney"
+        ))
+    ),
+    2 =>  array(
+        "image" => "http://placehold.jp/150x100.png",
+        "question" => "Open a jar of pickles",
+        "options" => array(
+        "a" => array(
+            "image" => "http://placehold.jp/150x100.png",
+            "option" => "Left loosey"
+        ),
+        "b" => array(
+            "image" => "http://placehold.jp/150x100.png",
+            "option" => "Righty tighty"
+        ),
+        "c" => array(
+            "image" => "http://placehold.jp/150x100.png",
+            "option" => "Smashy Bashy"
+        ))
+    ),
+    3 =>  array(
+        "image" => "http://placehold.jp/150x100.png",
+        "question" => "Dogs need to be fed with:",
+        "options" => array(
+        "a" => array(
+            "image" => "http://placehold.jp/150x100.png",
+            "option" => "cheese"
+        ),
+        "b" => array(
+            "image" => "http://placehold.jp/150x100.png",
+            "option" => "grapes"
+        ),
+        "c" => array(
+            "image" => "http://placehold.jp/150x100.png",
+            "option" => "chocolate"
+        ))
+    ),
+];
+
+foreach ($questions[$questionNum]["options"] as $key => $option) {
+    $id = $key.$questionNum;
+    $options .= '<div class="tile">
+        <label class="tile" for="'.$id.'">
+            <div>'.strtoupper($key).'</div>
+            <img src="'. $option["image"] .'">
+            <p>'.$option["option"].'</p>
+        </label>
+        <input type="radio" name="question'.$questionNum.'" id="'.$id.'" value="'.$id.'">
+    </div>';
+}
+$questionForm = '
+<form method="POST" action='.$_SERVER["PHP_SELF"].'>
+    <fieldset>
+        <legend>
+            <h2>Learn The Language of Sailing</h2>
+        </legend>
+        <div class="question">
+            <h3>Question '.$questionNum.': '. $questions[$questionNum]["question"].'</h3>
+             '. $options .'
+
+        </div>
+        <button class="btn">Submit</button>
+</form>
+';
+
+print($questionForm);

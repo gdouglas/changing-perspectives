06b479d1397def938ea89c70b2c615edecd64080
Graham <graham@Grahams-MacBook-Pro.local>
2020-02-12 11:06:38 -0800

    Added resources page

---
+++ en/ressources-resources/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/head.php" ?>
+    <title>Changing Perspectives | Resources</title>
+</head>
+
+<body>
+    <header>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/en-header.php" ?>
+    </header>
+    <main id="main" tabindex="-1" class="text-center">
+        <h1>Insructor Resources</h1>
+        <section class="intructor-resources text-left inline-block">
+            <h2>Navigation and Ship technology</h2>
+            <a href="/files/technology/navigation-learning-activity.pdf" target="_blank" download class="download-link"><img src="/images/icons/file-download-solid.svg" alt="download icon" class="icon">Navigation learning activity</a>
+            <a href="/files/technology/navigation-learning-activity-guidelines-for-teachers.pdf" target="_blank" download class="download-link"><img src="/images/icons/file-download-solid.svg" alt="download icon" class="icon">Navigation Learning Activity Guidelines For Teachers</a>
+        </section>
+    </main>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/en-footer.php" ?>
+</body>
+
+</html>

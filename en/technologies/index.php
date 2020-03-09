2ce401cf5958744dda40eae8c1dfa97a35f5cd1c
Graham <graham@Grahams-MacBook-Pro.local>
2020-03-09 10:15:17 -0700

    removed slider

--- en/technologies/index.php
+++ en/technologies/index.php
@@ -16,117 +16,58 @@
         <div class="pull-quote text-center">
             <h2 class="subtitle">What technologies did the Europeans need to get to the Pacific Northwest Coast?</h2>
         </div>
-        <div class="slider-container relative">
-            <ul class="slider">
-                <li id="article-1" class="slide active heading" tabindex="0">
-                    <h3 class="gold">How did European navigate the oceans?</h3>
-                </li>
-                <li id="article-2" class="slide" tabindex="0">
-                    <div style="padding:56.25% 0 0 0;position:relative;">
-                        <iframe id="vimeo1" title="European Navigation Video" src="https://player.vimeo.com/video/356042135?color=ee314a&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-                    </div>
-                    <script src="https://player.vimeo.com/api/player.js"></script>
-                    <h3 class="gold">Navigation</h3>
-                </li>
-                <li id="article-3" class="slide heading" tabindex="0">
-                    <h3 class="gold">What were the ships like the Europeans sailed in?</h3>
-                </li>
-                <li id="article-4" class="slide" tabindex="0">
-                    <div style="padding:56.25% 0 0 0;position:relative;">
-                        <iframe src="https://player.vimeo.com/video/356042272?color=ee314a&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-                    </div>
-                    <script src="https://player.vimeo.com/api/player.js"></script>
-                    <h3 class="gold">Ships</h3>
-                </li>
-                <li id="article-5" class="slide" tabindex="0">
-                    <figure>
-                        <img src="/images/sextant.jpg" alt="brass sextant">
-                        <figcaption>
-                            <h3 class="gold">Sextant</h3>
-                            <p>Brass <strong>sextant</strong> with five eyepieces, made by Jesse Ramsden, was used by Joseph Baker, Vancouver’s First Lieutenant and is on loan courtesy of Richard Baker. The sextant is a doubly reflecting navigation instrument with a graduated arc of 60° and a sighting mechanism, used for measuring the angular distances between objects and especially for taking altitudes in navigation.</p>
-                        </figcaption>
-                    </figure>
-                </li>
-                <li id="article-6" class="slide" tabindex="0">
-                    <figure>
-                        <img src="/images/telescope.jpg" alt="brass telescope">
-                        <figcaption>
-                            <h3 class="gold">Telescope</h3>
-                            <p>Two stage, leather-covered <strong>telescope</strong> made by G. or P. Dolland, inscribed "Capt. John. Baker Royal Navy" on loan courtesy of Richard Baker. Joseph Baker, Vancouver’s First Lieutenant . A telescope is an optical instrument which magnifies distant objects by arranging lenses or curved mirrors to collect and focus light.</p>
-                        </figcaption>
-                    </figure>
-                </li>
-                <li id="article-7" class="slide" tabindex="0">
-                    <figure>
-                        <img src="/images/chronometer.jpg" alt="brass chronometer">
-                        <figcaption>
-                            <h3 class="gold">Arnold Chronometer</h3>
-                            <p>John Arnold (maker) <strong>"Arnold 176" chronometer</strong> used by George Vancouver for 1790 expedition into Pacific Northwest. Completed in 1791 for the Board of Longitude, now in gimbal suspension with 3 tier mahogany case, purchased from Christie’s Auction House.</p>
-                            <p>A marine Chronometer is a timepiece that is precise and accurate enough to be used as a portable time standard, meaning it can be used to determine longitude by accurately measuring the time of a known fixed location. When first developed in the 18th century, it was a major technical achievement, as accurate knowledge of the time during sea voyage is necessary for navigation without the use of electronic or communication devices.</p>
-                        </figcaption>
-                    </figure>
-                </li>
-                <li id="article-8" class="slide heading" tabindex="0">
-                    <h3 class="gold">Can you help Captain Vancouver Sail his ship to Yuquot?</h3>
-                </li>
-                <li id="article-9" class="slide" tabindex="0">
-                    <figure class="text-center large">
-                        <img src="/images/ships-battle.jpg" height="400px" alt="brass chronometer">
-                        <a href="./game">Play Game</a>
-                    </figure>
-                </li>
-            </ul>
-            <div class="slider-controls">
-                <button id="prev" type="button" class="btn" disabled>Previous</button>
-                <button id="next" type="button" class="btn">Next</button>
+        <div class="card">
+            <h3 class="gold">How did European navigate the oceans?</h3>
+            <div style="padding:56.25% 0 0 0;position:relative;">
+                <iframe id="vimeo1" title="European Navigation Video" src="https://player.vimeo.com/video/356042135?color=ee314a&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
             </div>
-            <ol class="slider-navigation">
-                <li><a href="#article-1">
-                        <img class="slide-icon" src="/images/icons/question-circle.svg" alt="question icon">
-                    </a>
-                </li>
-                <li><a href="#article-2">
-                        <img class="slide-icon" src="/images/icons/video-solid.svg" alt="video icon">
-                    </a>
-                </li>
-                <li>
-                    <a href="#article-3">
-                        <img class="slide-icon" src="/images/icons/question-circle.svg" alt="question icon">
-                    </a>
-                </li>
-                <li>
-                    <a href="#article-4">
-                    <img class="slide-icon" src="/images/icons/video-solid.svg" alt="video icon">
-                    </a>
-                </li>
-                <li>
-                    <a href="#article-5">
-                        <img class="slide-icon" src="/images/icons/newspaper-regular.svg" alt="article icon">
-                    </a>
-                </li>
-                <li>
-                    <a href="#article-6">
-                        <img class="slide-icon" src="/images/icons/newspaper-regular.svg" alt="article icon">
-                    </a>
-                </li>
-                <li>
-                    <a href="#article-7">
-                        <img class="slide-icon" src="/images/icons/newspaper-regular.svg" alt="article icon">
-                    </a>
-                </li>
-                <li>
-                    <a href="#article-8">
-                        <img class="slide-icon" src="/images/icons/question-circle.svg" alt="question icon">
-                    </a>
-                </li>
-                <li>
-                    <a href="#article-9">
-                        <img class="slide-icon" src="/images/icons/gamepad-solid.svg" alt="game icon">
-                    </a>
-                </li>
-            </ol>
+            <script src="https://player.vimeo.com/api/player.js"></script>
         </div>
-
+        <div class="card">
+            <h3 class="gold">What were the ships like the Europeans sailed in?</h3>
+            <div style="padding:56.25% 0 0 0;position:relative;">
+                <iframe src="https://player.vimeo.com/video/356042272?color=ee314a&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+            </div>
+            <script src="https://player.vimeo.com/api/player.js"></script>
+        </div>
+        <div class="card">
+            <h3 class="gold">Can you help Captain Vancouver Sail his ship to Yuquot?</h3>
+            <figure class="text-center large">
+                <img src="/images/ships-battle.jpg" height="400px" alt="brass chronometer">
+                <a href="./game">Play Game</a>
+            </figure>
+        </div>
+        
+        <ul>
+            <li id="article-5" class="slide" tabindex="0">
+                <figure>
+                    <img src="/images/sextant.jpg" alt="brass sextant">
+                    <figcaption>
+                        <h3 class="gold">Sextant</h3>
+                        <p>Brass <strong>sextant</strong> with five eyepieces, made by Jesse Ramsden, was used by Joseph Baker, Vancouver’s First Lieutenant and is on loan courtesy of Richard Baker. The sextant is a doubly reflecting navigation instrument with a graduated arc of 60° and a sighting mechanism, used for measuring the angular distances between objects and especially for taking altitudes in navigation.</p>
+                    </figcaption>
+                </figure>
+            </li>
+            <li id="article-6" class="slide" tabindex="0">
+                <figure>
+                    <img src="/images/telescope.jpg" alt="brass telescope">
+                    <figcaption>
+                        <h3 class="gold">Telescope</h3>
+                        <p>Two stage, leather-covered <strong>telescope</strong> made by G. or P. Dolland, inscribed "Capt. John. Baker Royal Navy" on loan courtesy of Richard Baker. Joseph Baker, Vancouver’s First Lieutenant . A telescope is an optical instrument which magnifies distant objects by arranging lenses or curved mirrors to collect and focus light.</p>
+                    </figcaption>
+                </figure>
+            </li>
+            <li id="article-7" class="slide" tabindex="0">
+                <figure>
+                    <img src="/images/chronometer.jpg" alt="brass chronometer">
+                    <figcaption>
+                        <h3 class="gold">Arnold Chronometer</h3>
+                        <p>John Arnold (maker) <strong>"Arnold 176" chronometer</strong> used by George Vancouver for 1790 expedition into Pacific Northwest. Completed in 1791 for the Board of Longitude, now in gimbal suspension with 3 tier mahogany case, purchased from Christie’s Auction House.</p>
+                        <p>A marine Chronometer is a timepiece that is precise and accurate enough to be used as a portable time standard, meaning it can be used to determine longitude by accurately measuring the time of a known fixed location. When first developed in the 18th century, it was a major technical achievement, as accurate knowledge of the time during sea voyage is necessary for navigation without the use of electronic or communication devices.</p>
+                    </figcaption>
+                </figure>
+            </li>
+        </ul>
     </main>
     <?php include $_SERVER['DOCUMENT_ROOT'] . "/en-footer.php" ?>
 </body>

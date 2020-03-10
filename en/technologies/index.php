d281ef74cb4d1efa93ba403dcd360f9714c28bc7
Graham <graham@Grahams-MacBook-Pro.local>
2020-03-10 13:25:19 -0700

    Added an image galley

--- en/technologies/index.php
+++ en/technologies/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html class="no-js" lang="en">
+<script src="https://player.vimeo.com/api/player.js"></script>
 
 <head>
     <?php include $_SERVER['DOCUMENT_ROOT'] . "/head.php" ?>
@@ -16,58 +17,92 @@
         <div class="pull-quote text-center">
             <h2 class="subtitle">What technologies did the Europeans need to get to the Pacific Northwest Coast?</h2>
         </div>
-        <div class="card">
-            <h3 class="gold">How did European navigate the oceans?</h3>
-            <div style="padding:56.25% 0 0 0;position:relative;">
-                <iframe id="vimeo1" title="European Navigation Video" src="https://player.vimeo.com/video/356042135?color=ee314a&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-            </div>
-            <script src="https://player.vimeo.com/api/player.js"></script>
+        <div class="cards">
+            <label>
+                <input type="checkbox" />
+                <div class="card">
+                    <div class="front">
+                        <h3>How did European navigate the oceans?</h3>
+                    </div>
+                    <div class="back">
+                        <div style="position:relative;">
+                            <iframe id="vimeo1" title="European Navigation Video" src="https://player.vimeo.com/video/356042135?color=ee314a&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </label>
+
+            <label>
+                <input type="checkbox" />
+                <div class="card">
+                    <div class="front">
+                        <h3>What were the ships like the Europeans sailed in?</h3>
+                    </div>
+                    <div class="back">
+                        <div style="position:relative;">
+                            <iframe src="https://player.vimeo.com/video/356042272?color=ee314a&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </label>
+
+            <label>
+                <input type="checkbox" />
+                <div class="card">
+                    <div class="front">
+                        <h3>Can you help Captain Vancouver Sail his ship to Yuquot?</h3>
+                    </div>
+                    <div class="back">
+                        <a href="./game">Play Game</a>
+                    </div>
+                </div>
+            </label>
         </div>
-        <div class="card">
-            <h3 class="gold">What were the ships like the Europeans sailed in?</h3>
-            <div style="padding:56.25% 0 0 0;position:relative;">
-                <iframe src="https://player.vimeo.com/video/356042272?color=ee314a&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-            </div>
-            <script src="https://player.vimeo.com/api/player.js"></script>
-        </div>
-        <div class="card">
-            <h3 class="gold">Can you help Captain Vancouver Sail his ship to Yuquot?</h3>
-            <figure class="text-center large">
-                <img src="/images/ships-battle.jpg" height="400px" alt="brass chronometer">
-                <a href="./game">Play Game</a>
-            </figure>
-        </div>
-        
-        <ul>
-            <li id="article-5" class="slide" tabindex="0">
-                <figure>
-                    <img src="/images/sextant.jpg" alt="brass sextant">
-                    <figcaption>
-                        <h3 class="gold">Sextant</h3>
-                        <p>Brass <strong>sextant</strong> with five eyepieces, made by Jesse Ramsden, was used by Joseph Baker, Vancouver’s First Lieutenant and is on loan courtesy of Richard Baker. The sextant is a doubly reflecting navigation instrument with a graduated arc of 60° and a sighting mechanism, used for measuring the angular distances between objects and especially for taking altitudes in navigation.</p>
-                    </figcaption>
-                </figure>
-            </li>
-            <li id="article-6" class="slide" tabindex="0">
-                <figure>
-                    <img src="/images/telescope.jpg" alt="brass telescope">
-                    <figcaption>
-                        <h3 class="gold">Telescope</h3>
-                        <p>Two stage, leather-covered <strong>telescope</strong> made by G. or P. Dolland, inscribed "Capt. John. Baker Royal Navy" on loan courtesy of Richard Baker. Joseph Baker, Vancouver’s First Lieutenant . A telescope is an optical instrument which magnifies distant objects by arranging lenses or curved mirrors to collect and focus light.</p>
-                    </figcaption>
-                </figure>
-            </li>
-            <li id="article-7" class="slide" tabindex="0">
-                <figure>
-                    <img src="/images/chronometer.jpg" alt="brass chronometer">
-                    <figcaption>
-                        <h3 class="gold">Arnold Chronometer</h3>
-                        <p>John Arnold (maker) <strong>"Arnold 176" chronometer</strong> used by George Vancouver for 1790 expedition into Pacific Northwest. Completed in 1791 for the Board of Longitude, now in gimbal suspension with 3 tier mahogany case, purchased from Christie’s Auction House.</p>
-                        <p>A marine Chronometer is a timepiece that is precise and accurate enough to be used as a portable time standard, meaning it can be used to determine longitude by accurately measuring the time of a known fixed location. When first developed in the 18th century, it was a major technical achievement, as accurate knowledge of the time during sea voyage is necessary for navigation without the use of electronic or communication devices.</p>
-                    </figcaption>
-                </figure>
-            </li>
-        </ul>
+        <section id="gallery" class="gallery">
+            <h2>Artifacts of exploration</h2>
+            <ul>
+                <li>
+                    <figure id="sextant" class="relative">
+                        <a href="#sextant" class="open">
+                            <img src="/images/sextant.jpg" alt="brass sextant">
+                        </a>
+                        <a href="#gallery" class="close btn absolute top right">Close</a>
+                        <img src="/images/sextant.jpg" alt="brass sextant">
+                        <figcaption>
+                            <h3 class="gold">Sextant</h3>
+                            <p>Brass <strong>sextant</strong> with five eyepieces, made by Jesse Ramsden, was used by Joseph Baker, Vancouver’s First Lieutenant and is on loan courtesy of Richard Baker. The sextant is a doubly reflecting navigation instrument with a graduated arc of 60° and a sighting mechanism, used for measuring the angular distances between objects and especially for taking altitudes in navigation.</p>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li>
+                    <figure id="telescope" class="relative">
+                        <a href="#telescope" class="open">
+                            <img src="/images/telescope.jpg" alt="brass telescope">
+                        </a>
+                        <a href="#gallery" class="close btn absolute top right">Close</a>
+                        <img src="/images/telescope.jpg" alt="brass telescope">
+                        <figcaption>
+                            <h3 class="gold">Telescope</h3>
+                            <p>Two stage, leather-covered <strong>telescope</strong> made by G. or P. Dolland, inscribed "Capt. John. Baker Royal Navy" on loan courtesy of Richard Baker. Joseph Baker, Vancouver’s First Lieutenant . A telescope is an optical instrument which magnifies distant objects by arranging lenses or curved mirrors to collect and focus light.</p>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li>
+                    <figure id="chronometer" class="relative">
+                        <a href="#chronometer" class="open">
+                            <img src="/images/chronometer.jpg" alt="brass chronometer">
+                        </a>
+                        <a href="#gallery" class="close btn absolute top right">Close</a>
+                        <img src="/images/chronometer.jpg" alt="brass chronometer">
+                        <figcaption>
+                            <h3 class="gold">Arnold Chronometer</h3>
+                            <p>John Arnold (maker) <strong>"Arnold 176" chronometer</strong> used by George Vancouver for 1790 expedition into Pacific Northwest. Completed in 1791 for the Board of Longitude, now in gimbal suspension with 3 tier mahogany case, purchased from Christie’s Auction House.</p>
+                            <p>A marine Chronometer is a timepiece that is precise and accurate enough to be used as a portable time standard, meaning it can be used to determine longitude by accurately measuring the time of a known fixed location. When first developed in the 18th century, it was a major technical achievement, as accurate knowledge of the time during sea voyage is necessary for navigation without the use of electronic or communication devices.</p>
+                        </figcaption>
+                    </figure>
+                </li>
+            </ul>
+        </section>
     </main>
     <?php include $_SERVER['DOCUMENT_ROOT'] . "/en-footer.php" ?>
 </body>

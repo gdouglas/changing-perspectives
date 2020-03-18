f923e343b6729aeea5ea2309d6c647ad249537fa
Graham <graham@Grahams-MacBook-Pro.local>
2020-03-18 13:09:01 -0700

    Security fixes

--- en-header.php
+++ en-header.php
@@ -4,7 +4,7 @@
 <button id="nav-menu" class="btn" type="button">Menu</button>
 <div id="super-nav" class="super-nav">
     <a class="lang-link" href="/fra<?php echo substr($_SERVER['REQUEST_URI'], 3); ?>">Français</a>
-    <a class="gold logo" href="http://www.museevirtuel-virtualmuseum.ca/index-eng.jsp" target="_blank">
+    <a class="gold logo" href="http://www.museevirtuel-virtualmuseum.ca/index-eng.jsp" rel="noopener" target="_blank">
         <img src="/images/logos/vmc_eng_col_rev.svg" class="vmc-logo" alt="Virtual Museum of Canada">
     </a>
 </div>

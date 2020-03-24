320f5b35050c78a9084329c110b2ed353d038bf4
Graham <graham.douglas@ubc.ca>
2020-03-23 21:15:11 -0700

    mobile header

--- scripts/app.js
+++ scripts/app.js
@@ -1,48 +1,33 @@
 window.onload = function () {
     document.querySelector("html").classList.remove("no-js");
+    addNavListener();
+    addCardListeners();
 };
-    // //pause all Vimeo videos
-    // document.querySelectorAll("iframe").forEach((video) => {
-    //     stopVideo(video);
-    // })
-    // // check for iframe video and play it, only when scroll is done
-    // if (el.querySelector('iframe') && el.classList.contains('active')){
-    //     el.querySelector('iframe').contentWindow.postMessage('{"method":"play"}', '*');
-    // }
-
 
 // stop playing video
 var stopVideo = function ( iframe ) {
     iframe.contentWindow.postMessage('{"method":"pause"}', '*');
 };
 
-document.addEventListener('click', function (event) {
+function addNavListener() {
+    let navBtn = document.getElementById('nav-menu');
+    let nav = document.getElementById('top-nav');
+    let header = document.querySelector('header');
+    navBtn.addEventListener("click", function(){
+        header.classList.toggle('open');
+    });
+}
 
-	// If the clicked element doesn't have the right selector, bail
-	if (!event.target.matches('iframe')) return;
-    event.target.contentWindow.postMessage('{"method":"play"}', '*');
-	// Log the clicked element in the console
-	console.log(event.target);
+function addCardListeners(){
+    let vimeos = document.querySelectorAll('.vimeo');
+    let inputs = document.querySelectorAll("input[name=cards]");
 
-}, false);
-
-
-let vimeos = document.querySelectorAll('.vimeo');
-let inputs = document.querySelectorAll("input[name=cards]");
-
-[...inputs].forEach((radio) => {
-    radio.addEventListener("change", function(){
-        console.log(radio);
-        [...vimeos].forEach((vid) => {
-            console.log(vid)
+    [...inputs].forEach((radio) => {
+        radio.addEventListener("change", function(){
+            console.log(radio);
+            [...vimeos].forEach((vid) => {
+                stopVideo(vid);
+            });
         });
     });
-});
-
-
-[...vimeos].forEach((el) => {
-    let input = document.querySelector("input[value="+el.id);
-    
-    console.log(el);
-    
-});
+}

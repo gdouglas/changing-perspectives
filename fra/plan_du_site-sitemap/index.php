e17d1939a769892e31d2772b2b76bd00beead418
Graham <graham@Grahams-MacBook-Pro.local>
2020-04-15 14:27:52 -0700

    updated fra

--- fra/plan_du_site-sitemap/index.php
+++ fra/plan_du_site-sitemap/index.php
@@ -12,11 +12,17 @@
     </header>
     <main id="main" tabindex="-1" class="text-left">
         <ul>
-            <li><a href="/fra/home/" rel="noopener noreferrer">Home</a></li>
-            <li><a href="/fra/context/" rel="noopener noreferrer">Context</a></li>
-            <li><a href="/fra/expedition/" rel="noopener noreferrer">Vancouver Expedition</a></li>
-            <li><a href="/fra/legacies/" rel="noopener noreferrer">Vancouver Legacies</a></li>
-            <li><a href="/fra/technologies/" rel="noopener noreferrer">Navigation and Ship Technology</a></li>
+            <li><a href="/fra/debut-home/" rel="noopener">Home</a></li>
+            <li><a href="/fra/contexte-context/" rel="noopener">The Story Before Vancouver</a></li>
+            <li><a href="/fra/expedition/" rel="noopener">Vancouver's Story</a></li>
+            <li><a href="/fra/heritage-legacies/" rel="noopener">Stories from Yuquot</a></li>
+            <li><a href="/fra/technologies/" rel="noopener">European Navigation and Ship Technology</a></li>
+            <li><a href="/fra/sur-about/" rel="noopener">About</a></li>
+            <li><a href="/fra/ressources-resources/" rel="noopener">Teacher Resources</a></li>
+            <li><a href="/fra/credits/" rel="noopener">Credits</a></li>
+            <li><a href="/fra/plan_du_site-sitemap/" rel="noopener">Sitemap</a></li>
+            <li><a href="/fra/droit_dauteur-copyright/" rel="noopener">Copyright</a></li>
+            <li><a href="/fra/commentaires-feedback/" rel="noopener">Send us feedback</a></li>
         </ul>
     </main>
     <?php include $_SERVER['DOCUMENT_ROOT']."/fra-footer.php" ?>

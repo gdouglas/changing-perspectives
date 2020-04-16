3e629b0249f4621cbd37fe4be435cef0e2b51cb1
Graham <graham@Grahams-MacBook-Pro.local>
2020-04-15 22:28:45 -0700

    Transcripts, stop video on card flip

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,15 @@
 window.onload = function () {
     document.querySelector("html").classList.remove("no-js");
     addNavListener();
-    addCardListeners();
+    // addCardListeners();
     getVideos();
-    addTranscripts();
+    // addTranscripts();
 };
 
 // stop playing video
-var stopVideo = function (iframe) {
-    iframe.contentWindow.postMessage('{"method":"pause"}', "*");
-};
+// var stopVideo = function (iframe) {
+//     iframe.contentWindow.postMessage('{"method":"pause"}', "*");
+// };
 
 function addNavListener() {
     let navBtn = document.getElementById("nav-menu");
@@ -37,73 +37,75 @@ function getVideos() {
     if (players.length < 1) {
         return;
     }
-    let index = 0;
-    bufferVideos(players, index);
-
-    // for (let i = 0; i < players.length; i++) {
-    //     let vim = new Vimeo.Player(players[i]);
-    //     vim.setVolume(0);
-    //     vim.play().then(
-    //         function(value) {
-    //             console.log(value);
-    //             vim.pause();
-    //         }
-    //     );
-    // }
+    // let index = 0;
+    // bufferVideos(players, index);
 }
-function bufferVideos(players, index) {
-    console.log(players.length, index);
-    let playerElement = players[index];
-    let vim = new Vimeo.Player(playerElement);
+// function bufferVideos(players, index) {
+//     console.log(players.length, index);
+//     let playerElement = players[index];
+//     let vim = new Vimeo.Player(playerElement);
 
-    console.log("player is ", playerElement);
+//     console.log("player is ", playerElement);
 
-    playerElement.addEventListener("keydown", function (e) {
-        let key = e.key.toLowerCase();
-        console.log(key);
-        if (key === "escape" || key === "esc") {
-            console.log("pressed escape");
-            vim.pause();
-        }
-    });
+//     playerElement.addEventListener("keydown", function (e) {
+//         let key = e.key.toLowerCase();
+//         console.log(key);
+//         if (key === "escape" || key === "esc") {
+//             console.log("pressed escape");
+//             vim.pause();
+//         }
+//     });
 
-    // console.log(vim);
-    // // mute to enable autoplay
-    // let vidEnd = vim.getDuration();
-    // vim.setLoop(1);
-    // vim.setVolume(0).then(
-    //     vim.play()
-    // ).then(
-    //     function(value) {
-    //         console.log(value);
-    //         vim.pause()
-    //         .then(function(){
-    //             vim.setVolume(1);
-    //             vim.setCurrentTime(vidEnd);
-    //             index++;
-    //             if (index === players.length) {
-    //                 console.log("the end");
-    //                 return;
-    //             } else {
-    //                 console.log("keep going");
-    //                 bufferVideos(players, index);
-    //             }
-    //         });
-    //     }
-    // );
-}
+//     // console.log(vim);
+//     // // mute to enable autoplay
+//     // let vidEnd = vim.getDuration();
+//     // vim.setLoop(1);
+//     // vim.setVolume(0).then(
+//     //     vim.play()
+//     // ).then(
+//     //     function(value) {
+//     //         console.log(value);
+//     //         vim.pause()
+//     //         .then(function(){
+//     //             vim.setVolume(1);
+//     //             vim.setCurrentTime(vidEnd);
+//     //             index++;
+//     //             if (index === players.length) {
+//     //                 console.log("the end");
+//     //                 return;
+//     //             } else {
+//     //                 console.log("keep going");
+//     //                 bufferVideos(players, index);
+//     //             }
+//     //         });
+//     //     }
+//     // );
+// }
 function stopVimeo(vim) {
-    console.log("stop");
-    vim.pause();
+    // console.log("stop");
+    // vim.pause();
     // vim.setCurrentTime(100);
 }
-
-function addTranscripts() {
-    let buttons = document.querySelectorAll(".card-button");
-    console.log(buttons);
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", function (e) {
-            console.log(e.currentTarget.closest("iframe"));
-        });
+let cards = document.querySelectorAll("input[name='cards']");
+let players = document.querySelectorAll(".vimeo");
+let vimeoPlayers = [];
+for (let i = 0; i < players.length; i++) {
+    let vim = new Vimeo.Player(players[i]);
+    vimeoPlayers.push(vim);
+}
+if (cards.length > 0) {
+    for (var i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("change", function(){
+            vimeoPlayers.forEach(vim => vim.pause())
+        })
     }
 }
+// function addTranscripts() {
+//     let buttons = document.querySelectorAll(".card-button");
+//     console.log(buttons);
+//     for (let i = 0; i < buttons.length; i++) {
+//         buttons[i].addEventListener("click", function (e) {
+//             console.log(e.currentTarget.closest("iframe"));
+//         });
+//     }
+// }

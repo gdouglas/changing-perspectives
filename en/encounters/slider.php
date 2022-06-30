0fd0572ba9f700a18f5c86bad339d2b2d2071bc8
Graham Douglas <graham.douglas@ubc.ca>
2022-06-30 16:50:00 -0700

    changed sliders to horizontal

--- en/encounters/slider.php
+++ en/encounters/slider.php
@@ -1,11 +1,14 @@
 <section class="comic-wrapper">
-    <ul class="inline">
-        <li><img src="/images/encounters/encounters-title.png" alt="" /></li>
-        <li><img src="/images/encounters/encounters1.png" alt="" /></li>
-        <li><img src="/images/encounters/encounters2.png" alt="" /></li>
-        <li><img src="/images/encounters/encounters3.png" alt="" /></li>
-        <li><img src="/images/encounters/encounters4.png" alt="" /></li>
-        <li><img src="/images/encounters/encounters5.png" alt="" /></li>
-        <li><img src="/images/encounters/encounters6.png" alt="" /></li>
-    </ul>
+    <div class="panels">
+        <div><img src="/images/encounters/encounters-title.png" alt="" /></div>
+        <div><img src="/images/encounters/encounters1.png" alt="" /></div>
+        <div><img src="/images/encounters/encounters2.png" alt="" /></div>
+        <div><img src="/images/encounters/encounters3.png" alt="" /></div>
+        <div><img src="/images/encounters/encounters4.png" alt="" /></div>
+        <div><img src="/images/encounters/encounters5.png" alt="" /></div>
+        <div><img src="/images/encounters/encounters6.png" alt="" /></div>
+    </div>
 </section>
+<div>
+    <p class="text-center centered-margin">Excerpt from <a href="https://arsenalpdivp.com/Books/T/The-500-Years-of-Indigenous-Resistance-Comic-Book-Revised-and-Expanded" target="_blank" rel="noopener">500 Years of Indigenous Resistance</a></p>
+</div>
--- en/legacies/slider.php
+++ en/legacies/slider.php
@@ -1,12 +1,15 @@
 <section class="comic-wrapper">
-    <ul class="inline">
-        <li><img src="/images/colonization/colonization-title.png" alt="" /></li>
-        <li><img src="/images/colonization/colonization1.png" alt="" /></li>
-        <li><img src="/images/colonization/colonization2.png" alt="" /></li>
-        <li><img src="/images/colonization/colonization3.png" alt="" /></li>
-        <li><img src="/images/colonization/colonization4.png" alt="" /></li>
-        <li><img src="/images/colonization/colonization5.png" alt="" /></li>
-        <li><img src="/images/colonization/colonization6.png" alt="" /></li>
-        <li><img src="/images/colonization/colonization7.png" alt="" /></li>
-    </ul>
+    <div class="panels">
+        <div><img src="/images/colonization/colonization-title.png" alt="" /></div>
+        <div><img src="/images/colonization/colonization1.png" alt="" /></div>
+        <div><img src="/images/colonization/colonization2.png" alt="" /></div>
+        <div><img src="/images/colonization/colonization3.png" alt="" /></div>
+        <div><img src="/images/colonization/colonization4.png" alt="" /></div>
+        <div><img src="/images/colonization/colonization5.png" alt="" /></div>
+        <div><img src="/images/colonization/colonization6.png" alt="" /></div>
+        <div><img src="/images/colonization/colonization7.png" alt="" /></div>
+    </div>
 </section>
+<div>
+    <p class="text-center centered-margin">Excerpt from <a href="https://arsenalpdivp.com/Books/T/The-500-Years-of-Indigenous-Resistance-Comic-Book-Revised-and-Expanded" target="_blank" rel="noopener">500 Years of Indigenous Resistance</a></p>
+</div>

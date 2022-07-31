747b2acabc27ad99f40a2f106002325956a2b5f6
Graham Douglas <graham.douglas@ubc.ca>
2022-07-31 16:03:17 -0700

    homepage content update

--- en/debut-home/index.php
+++ en/debut-home/index.php
@@ -14,29 +14,22 @@
         <?php require $_SERVER['DOCUMENT_ROOT'] . '/en-header.php'; ?>
     </header>
     <main id="main" role="main" tabindex="-1">
-        <div class="home-intro">
+        <div class="home-intro-wrapper">
             <h1>Relations, Encounters and Legacies from the time of the Vancouver Expedition</h1>
-
-            <div class="text-left">
-                <p>
-                    Welcome, this online virtual exhibit revisits the history of European colonization in what is now thePacific Northwest.
-                </p>
-                <section class="site-advisory">
-                    <p>This online virtual exhibit is produced by the Vancouver Maritime Museum which is fortunate to be hosted on the traditional territories of the xʷməθkwəy̓əm (Musqueam), Skwxwú7mesh (Squamish), and Səl̓ílwətaʔ/Selilwitulh (Tsleil-Waututh) Nations in Vancouver British Columbia, Canada.</p>
-                    <br>
-                    <p>The Changing Perspectives exhibit would also like to recognize our Haíɫzaqv (Heiltsuk) and Mowachaht/Muchalaht collaborators in Bella Bella and Yuquot, respectively. As hosts and partners, they shaped our understanding of the legacies of Captain Vancouver’s expedition for their communities and territories.</p>
-                </section>
-            </div>
-            <div class="home-advisory">
+            <section class="site-intro">
+                <div class="text-left">
+                    <p>
+                        Welcome. The Changing Perspectives exhibit revisits the histories of European colonization of Indigenous territories in what is now the Pacific Northwest. It is produced by the Vancouver Maritime Museum which is fortunate to be hosted on the traditional territories of the xʷməθkwəy̓əm (Musqueam), Skwxwú7mesh (Squamish), and Səl̓ílwətaʔ/Selilwitulh (Tsleil-Waututh) Nations in Vancouver British Columbia, Canada.
+                    </p>
+                    <p>
+                        The Vancouver Maritime Museum would also like to recognize our Haíɫzaqv (Heiltsuk) and Mowachaht/Muchalaht collaborators in Bella Bella and Yuquot, respectively. As hosts and partners, they shaped our understanding of the legacies of Captain Vancouver’s expedition for their communities and territories.
+                    </p>
+                </div>
+            </section>
+            <div class="home-artist-acknowledgement">
                 Some of the images on this website are by non-Aboriginal artists from the past and present. They are based on their perspective of early contact and not on Indigenous knowledge traditions. It is important to remember that they tell us as much about the artists themselves as they do about what really happened in the late 18th and early 19th centuries.
             </div>
         </div>
-        <!-- <div class="bg-video-wrapper">
-            <iframe title="European Navigation Video"
-                src="https://player.vimeo.com/video/356042135?background=true"
-                allow="autoplay;">
-            </iframe>
-        </div>         -->
         <div class="bg-video-wrapper">
             <video id="bg-vid"
                 width="100%"

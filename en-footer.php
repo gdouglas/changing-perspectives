fe5f3c3fd6c873961e4856b8e026b42919b79f8c
Graham Douglas <graham.douglas@ubc.ca>
2022-07-31 16:14:12 -0700

    limited footer width

--- en-footer.php
+++ en-footer.php
@@ -1,27 +1,29 @@
 <footer>
-    <div class="col info">
-        <h3>Info</h3>
-        <div class="content">
-            <a href="/en/sur-about/">About</a>
-            <a href="/en/credits/">Credits</a>
-            <a href="https://www.digitalmuseums.ca/privacy-notice/" target="_blank" rel="noopener">DMC Privacy Policy</a>
-            <a href="/en/plan_du_site-sitemap/">Sitemap</a>
-            <a href="/en/commentaires-feedback/">Send us feedback</a>
+    <div class="footer-container">
+        <div class="col info">
+            <h3>Info</h3>
+            <div class="content">
+                <a href="/en/sur-about/">About</a>
+                <a href="/en/credits/">Credits</a>
+                <a href="https://www.digitalmuseums.ca/privacy-notice/" target="_blank" rel="noopener">DMC Privacy Policy</a>
+                <a href="/en/plan_du_site-sitemap/">Sitemap</a>
+                <a href="/en/commentaires-feedback/">Send us feedback</a>
+            </div>
         </div>
-    </div>
-    <div class="col resources">
-        <h3>Resources</h3>
-        <div class="content">
-            <a href="/en/ressources-resources/">Learning Resources</a>
-            <a href="/en/glossary/">Glossary</a>
+        <div class="col resources">
+            <h3>Resources</h3>
+            <div class="content">
+                <a href="/en/ressources-resources/">Learning Resources</a>
+                <a href="/en/glossary/">Glossary</a>
+            </div>
         </div>
-    </div>
-    <div class="col vmm">
-        <a href="https://vanmaritime.com"><img src="/images/logos/vmm_logo_white.png" alt="Vancouver Maritime Museum" /></a>
-        <div class="content">
-            <address>
-                <p>1905 Ogden Ave.<br> Vancouver, BC<br> V6J 1A3</p>
-            </address>
+        <div class="col vmm">
+            <a href="https://vanmaritime.com"><img src="/images/logos/vmm_logo_white.png" alt="Vancouver Maritime Museum" /></a>
+            <div class="content">
+                <address>
+                    <p>1905 Ogden Ave.<br> Vancouver, BC<br> V6J 1A3</p>
+                </address>
+            </div>
         </div>
     </div>
     <div class="sub-footer">

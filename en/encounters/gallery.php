96b9f33a7a892488a0aedb411500204400be0367
Graham Douglas <graham.douglas@ubc.ca>
2022-09-08 15:06:53 -0700

    gallery content and layout

--- en/encounters/gallery.php
+++ en/encounters/gallery.php
@@ -8,11 +8,12 @@
             <figure>
                 <img src="/images/encounters/sea-otter-pelts-thumb.jpg" alt="Two black and white photos. In one, a man wearing a bowler hat poses against a shiplap wall with three oval pelts. In the other a man stands beside one long pelt and a small baby pelt. In pen the first image has 'Sea Otter Skin. 1892 Ounakaska' written. The other has 'Sea Otter Skin Ounalaska.'">
                 <figcaption>
-                    <p>Sea otter skins, Unalaska, Alaska, 1892</p>
-                    <p>Archival Photographer Stefan Claesson</p>
-                    <p>Gulf of Maine Cod Project, NOAA National Marine Sanctuaries; Courtesy of National Archives</p>
-                    <p>These 1892 photos from Alaska show how large sea otter pelts are. They have a soft, thick coat of fur to keep them warm in the Pacific Ocean.</p>
-                    <p>Indigenous peoples on the coast have hunted sea otters for food and fur for thousands of years. They traded the pelts with overseas fur traders starting in the 1700s. Sailors found the pelts beautiful and rare, and could sell them in China for a small fortune. Within 200 years, sea otters were almost hunted to extinction on the coast.</p>
+                    <p>Sea otter skins, ca.1892. Archival Photographer Stefan Claesson. Unalaska, Alaska, Gulf of Maine Cod
+Project, NOAA National Marine Sanctuaries; Courtesy of National Archives. These photos show how
+large and beautiful sea otter pelts are. They have a soft, thick coat of fur to keep them warm in the
+Pacific Ocean. Indigenous peoples traded the pelts with overseas fur traders starting in the 1700s.
+Sailors could sell the pelts in China for a small fortune. Within 200 years, sea otters were almost hunted
+to extinction on the coast.</p>
                 </figcaption>
             </figure>
             
@@ -23,11 +24,14 @@
             <figure>
                 <img src="/images/encounters/nuu-chah-nulth-fish-hook-thumb.jpg" alt="A thin peice of wood curves into a 'U' shape with a curve on one end and a sharp tusk pointed inwards on the other. The tusk is attached with leather and twine. The other end has leather wound around it for holding a line.">
                 <figcaption>
-                <p>Halibut fish hook, ca. 1790s.</p>
-                <p>Courtesy of The British Museum</p>
-                <p>Catalogue Number: Am, VAN.131.b</p>
-                <p>Nuu-chah-nulth designed this hook to catch halibut, a large flatfish that swims along the ocean floor. The hook is bent from wood. It is 12.5cm long and 12.5cm wide.  A piece of bait would be tied onto the bone barb. Then the hook would be lowered down into the water on a rope.</p>
-                <p>Captain Vancouver's crew were often hungry because they didn't know how to hunt and fish on the Pacific Northwest Coast. They traded for Indigenous fishing gear, but also relied on Indigenous communities for food and fresh water. This fish hook was collected by the surgeon's mate on the HMS Discovery, George Goodman Hewett, from the Mowachaht/Muchalaht First Nation at Nootka Sound. </p>
+                <p>Halibut fish hook, ca. 1790s. Courtesy of The British Museum Catalogue Number: Am, VAN.131.b. Nuu-
+chah-nulth designed this hook to catch halibut, a large flatfish that swims along the ocean floor. The
+hook is bent from wood. It is 12.5cm long and 12.5cm wide. A piece of bait would be tied onto the bone
+barb. Then the hook would be lowered down into the water on a rope. Captain Vancouver&#39;s crew were
+often hungry because they didn&#39;t know how to hunt and fish on the Pacific Northwest Coast. They
+traded for Indigenous fishing gear, but also relied on Indigenous communities for food and fresh water.
+This fish hook was collected by the surgeon&#39;s mate on the HMS Discovery, George Goodman Hewett,
+from the Mowachaht/Muchalaht First Nation at Yuquot.</p>
             </figcaption>
             </figure>
         </a>
@@ -37,9 +41,10 @@
             <figure>
                 <img src="/images/encounters/cat-o-nine-tails-thumb.jpg" alt="A wooden rod with a short leather strap bound to it's end. The leather strap has nine cords tied to it's end. The cords have multiple knots tied to them. The bottom of the rod shows heavy signs of wear from being handled.">
                 <figcaption id="cat-o-nine">
-                    <p>Cat-o'-nine tails whip, ca. 1700-1850</p>
-                    <p>Courtesy of Wellcome Collection </p>
-                    <p>A cat-o'-nine tails was a whip used by the British Royal Navy to punish sailors. It consists of nine pieces of cord each tied with a series of knots. Sailors could receive up to 24 whip lashes across their backs at a time. Like other Royal Navy commanders, Captain Vancouver regularly punished his crew this way.</p>
+                    <p>Cat-o&#39;-nine tails whip, ca. 1700-1850. Courtesy of Wellcome Collection. A cat-o&#39;-nine tails was a whip
+used by the British Royal Navy to punish sailors. It consists of nine pieces of cord each tied with a series
+of knots. Sailors could receive up to 24 whip lashes across their bare backs at a time. Like other Royal
+Navy commanders, Captain Vancouver regularly punished his crew this way.</p>
                 </figcaption>
             </figure>
         </a>
--- en/navigation/gallery.php
+++ en/navigation/gallery.php
@@ -7,7 +7,7 @@
                 <figure>
                     <img src="/images/navigation/sextant-thumb.jpg" alt="A brass sextant. It is triangular with an arc on the bottom.">
                     <figcaption>
-                        <p>Brass <strong>sextant</strong> with five eyepieces, made by Jesse Ramsden, was used by Joseph Baker, Vancouver&rsquo;s First Lieutenant and is on loan courtesy of Richard Baker. The sextant is a doubly reflecting navigation instrument with a graduated arc of 60° and a sighting mechanism, used for measuring the angular distances between objects and especially for taking altitudes in navigation.</p>
+                        <p>Brass sextant with five eyepieces, ca. 1790s. Made by Jesse Ramsden, was used by Joseph Baker, Vancouver&rsquo;s First Lieutenant and is on loan to the Vancouver Maritime Museum courtesy of Richard Baker. The sextant is a doubly reflecting navigation instrument with a graduated arc of 60° and a sighting mechanism, used for measuring the angular distances between objects and especially for taking altitudes in navigation.</p>
                     </figcaption>
                 </figure>
             </a>
@@ -17,7 +17,7 @@
                 <figure>
                     <img src="/images/navigation/telescope-thumb.jpg" alt="A telescope with a brass tip holding the lens and eyepiece. The body is shiny weathered leather.">
                     <figcaption>
-                        <p>Two stage, leather-covered <strong>telescope</strong> made by G. or P. Dolland, inscribed "Capt. John. Baker Royal Navy" on loan courtesy of Richard Baker. Joseph Baker, Vancouver&rsquo;s First Lieutenant . A telescope is an optical instrument which magnifies distant objects by arranging lenses or curved mirrors to collect and focus light.</p>
+                        <p>Two stage, leather-covered telescope, ca. 1790s. Made by G. or P. Dolland, inscribed &quot;Capt. John. Baker Royal Navy&quot; on loan to the Vancouver Maritime Museum courtesy of Richard Baker. Joseph Baker was Vancouver&rsquo;s First Lieutenant. A telescope is an optical instrument which magnifies distant objects by arranging lenses or curved mirrors to collect and focus light.</p>
                     </figcaption>
                 </figure>
             </a>
@@ -27,8 +27,7 @@
                 <figure>
                     <img src="/images/navigation/chronometer-thumb.jpg" alt="A wooden box opened with a clock like mechanism inside of it.">
                     <figcaption>
-                        <p>John Arnold (maker) <strong>"Arnold 176" chronometer</strong> used by George Vancouver for 1790 expedition into Pacific Northwest. Completed in 1791 for the Board of Longitude, now in gimbal suspension with 3 tier mahogany case, purchased from Christie&rsquo;s Auction House.</p>
-                        <p>A marine chronometer is a timepiece that is precise and accurate enough to be used as a portable time standard, meaning it can be used to determine longitude by accurately measuring the time of a known fixed location. When first developed in the 18th century, it was a major technical achievement, as accurate knowledge of the time during sea voyage is necessary for navigation without the use of electronic or communication devices.</p>
+                        <p>&quot;Arnold 176&quot; chronometer, ca. 1791. Made by John Arnold and used by George Vancouver for his expedition to the Pacific Northwest. Owned by the Vancouver Maritime Museum. A marine chronometer is a timepiece that is precise and accurate enough to be used as a portable time standard, meaning it can be used to determine longitude by accurately measuring the time of a known fixed location. When first developed in the 18th century, it was a major technical achievement, as accurate knowledge of the time during sea voyage is necessary for navigation without the use of electronic or communication devices.</p>
                     </figcaption>
                 </figure>
             </a>

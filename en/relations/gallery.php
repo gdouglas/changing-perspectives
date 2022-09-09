003bcffe0838b41ee2f8087d694e815cff8156c2
Graham Douglas <graham.douglas@ubc.ca>
2022-09-09 14:22:27 -0700

    Gallery caption edits

--- en/relations/gallery.php
+++ en/relations/gallery.php
@@ -6,14 +6,7 @@
         <a href="/images/relations/glass-trade-beads.jpg" class="lightbox">
         <figure>
             <img src="/images/relations/glass-trade-beads-thumb.jpg" alt="Blue glass beads with a hole through the middle for string.">
-            <figcaption>
-                    <p>Beads, ca. 1800s. Courtesy of Fort Vancouver National Historic Site Catalogue Number: FOVA 44083.
-Small glass beads, like these, were valuable on the Pacific Northwest Coast in the late 1700s. Captain
-Vancouver&#39;s crew brought beads to trade with Indigenous peoples in exchange for sea otter pelts and
-for food. Indigenous communities viewed glass beads as a symbol of wealth. They used them to
-decorate clothing, replacing traditional shell, stone, and bone beads. Beading continues to be an
-important Indigenous artform.</p>
-            </figcaption>
+            <figcaption><p>Beads, ca. 1800s. Courtesy of Fort Vancouver National Historic Site Catalogue Number: FOVA 44083. Small glass beads, like these, were valuable on the Pacific Northwest Coast in the late 1700s. Captain Vancouver&#39;s crew brought beads to trade with Indigenous peoples in exchange for sea otter pelts and for food. Indigenous communities viewed glass beads as a symbol of wealth. They used them to decorate clothing, replacing traditional shell, stone, and bone beads. Beading continues to be an important Indigenous artform.</p></figcaption>
         </figure>
         </a>
     </li>
@@ -21,12 +14,7 @@ important Indigenous artform.</p>
         <a href="/images/relations/trade-axe.jpg" class="lightbox">
             <figure>
                 <img src="/images/relations/trade-axe-thumb.jpg" alt="Rusty metal axe head. Has a hatchet style design.">
-                <figcaption>
-                    <p>Axe, ca. late 1700s. Courtesy of Fort Langley National Historic Site. Overseas fur-traders brought many
-iron goods on their ships in the 1700s, such as axes, pots, hammers, and knives. They traded these iron
-goods for otter pelts and food from the Indigenous communities. Indigenous peoples used these iron
-tools for hunting, cooking and carving.</p>
-                        </figcaption>
+                <figcaption><p>Axe, ca. late 1700s. Courtesy of Fort Langley National Historic Site. Overseas fur-traders brought many iron goods on their ships in the 1700s, such as axes, pots, hammers, and knives. They traded these iron goods for otter pelts and food from the Indigenous communities. Indigenous peoples used these iron tools for hunting, cooking and carving.</p></figcaption>
             </figure>
         </a>
     </li>
@@ -34,17 +22,7 @@ tools for hunting, cooking and carving.</p>
         <a href="/images/relations/trade-musket.jpg" class="lightbox">
             <figure>
                 <img src="/images/relations/trade-musket-thumb.jpg" alt="A rifle with a wooden stock.">
-                <figcaption>
-                <p>Musket, ca. 1871. Made by J.E. Barnett and Sons Courtesy of The Manitoba Museum. This 1871 musket
-is a later model of those exchanged by fur traders for pelts in the late 1700s. Traditional Indigenous
-bows, arrows and clubs were often better weapons. These muskets were breakable, took a long time to
-reload, and were not accurate. However, muskets became a symbol of wealth on the coast, often gifted
-at potlatch feasts. Captain Vancouver did not allow the trading of weapons because he feared they
-would be used against his crew. They experienced several violent encounters: the Hawaiians killed one
-crewman at O&#39;ahu in 1792 and Tlingit warriors attacked the expedition the following year. Two of the
-crew were wounded and as many as 9 Tlingit warriors were killed. British Royal Navy expeditions were
-heavily armed with handguns and cannons.</p>
-                        </figcaption>
+                <figcaption><p>Musket, ca. 1871. Made by J.E. Barnett and Sons Courtesy of The Manitoba Museum. This 1871 musket is a later model of those exchanged by fur traders for pelts in the late 1700s. Traditional Indigenous bows, arrows and clubs were often better weapons. These muskets were breakable, took a long time to reload, and were not accurate. However, muskets became a symbol of wealth on the coast, often gifted at potlatch feasts. Captain Vancouver did not allow the trading of weapons because he feared they would be used against his crew. They experienced several violent encounters: the Hawaiians killed one crewman at O&#39;ahu in 1792 and Tlingit warriors attacked the expedition the following year. Two of the crew were wounded and as many as nine Tlingit warriors were killed. British Royal Navy expeditions were heavily armed with handguns and cannons.</p></figcaption>
             </figure>
         </a>
     </li>

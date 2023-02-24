c7b7f44b7eb8d560ef05d8ab07e61ac76bb4d12e
Graham Douglas <graham.douglas@ubc.ca>
2023-02-23 22:05:20 -0800

    french captions on comics

--- fr/legs/slider.php
+++ fr/legs/slider.php
@@ -1,72 +1,101 @@
 <section class="comic-wrapper">
     <div class="panels">
         <div id="content-advisory" class="comic-advisory">
-            <p>Cette bande dessinée est tirée de « L’album de 500 ans de résistance autochtone » (The 500 Years of Indigenous Resistance Comic Book) créé par l’artiste et historien Kwakwaka’wakw Gord Hill.</p>
+            <p>Cette bande dessinée est tirée de «&nbsp;L’album de 500 ans de résistance autochtone&nbsp;» (The 500 Years of Indigenous Resistance Comic Book) créé par l’artiste et historien Kwakwaka’wakw Gord Hill.</p>
             <p><strong>AVERTISSEMENT: </strong>Certaines images sont de nature très crue et peuvent être troublantes.</p>
         </div>
         <figure id="one">
             <img src="/images/colonization/colonization-title.png" alt="title-panel" aria-describedby="panel-one-transcript" />
+            <figcaption>Comment les Européens ont-ils colonisé la côte nord-ouest du Pacifique?</figcaption>
         </figure>
         <figure id="two">
             <img src="/images/colonization/colonization1.png" alt="Panel 1 of 8" aria-describedby="panel-two-transcript" />
+            <figcaption>
+                Dans les années 1820, la Compagnie de la Baie d’Hudson constituait le principal pouvoir colonial de la région, même si le commerce des fourrures était en déclin. La compagnie détenait de nombreux forts et de nombreux bateaux.
+                <br>
+                En 1828, un village Klallam, dans l’État de Washington, fut détruit par une canonnière de la Compagnie.
+            </figcaption>
         </figure>
         <figure id="three">
             <img src="/images/colonization/colonization2.png" alt="Panel 2 of 8" aria-describedby="panel-three-transcript" />
+            <figcaption>En 1849, l’île de Vancouver fut déclarée colonie officielle avec la CBH agissant à titre de gouvernement.</figcaption>
         </figure>
         <figure id="four">
             <img src="/images/colonization/colonization3.png" alt="Panel 3 of 8" aria-describedby="panel-four-transcript" />
+            <figcaption>
+                <p>Au même moment, l’établissement de colons et la vente de terres étaient devenus des sujets cruciaux pour le gouvernement…</p>
+                <p>«&nbsp;Les Kwakiutl sont toujours des chasseurs de têtes?&nbsp;»</p>
+                <p>«&nbsp;Oui monsieur…&nbsp;»</p>
+                <p>Pendant 30 ans, les canonnières de la Marine royale ont été utilisées pour servir le colonialisme britannique et contrôler les tribus côtières.</p>
+            </figcaption>
         </figure>
         <figure id="five">
             <img src="/images/colonization/colonization4.png" alt="Panel 4 of 8" aria-describedby="panel-five-transcript" />
+            <figcaption>
+                <p>Les canonnières servaient de forces militaire et policière…</p>
+                <p>Elles étaient lourdement armées avec 50 canons et roquettes ainsi que des soldats de la Marine Royale.</p>
+            </figcaption>
         </figure>
         <figure id="six">
             <img src="/images/colonization/colonization5.png" alt="Panel 6 of 8" aria-describedby="panel-six-transcript" />
+            <figcaption>
+                <p>En 1862, une épidémie de variole se déclara à Victoria. Les autorités coloniales ont forcé une centaine d’Autochtones à s’en aller.</p>
+                <p>Infectés, ils sont retournés dans leurs villages et ont répandu la maladie. On estime qu’une personne sur trois est morte en 2 ans. L’épidémie s’est déclarée avec l’arrivée d’un plus grand nombre de colons européens.</p>
+            </figcaption>
         </figure>
         <figure id="seven">
             <img src="/images/colonization/colonization6.png" alt="Panel 7 of 8" aria-describedby="panel-seven-transcript" />
+            <figcaption>
+                <p>En 1871, la C.-B. est entrée dans le Canada. En 1874, la Loi relative aux terres de la C.-B. fut adoptée pour ouvrir le territoire à la colonisation.</p>
+                <p>Le Canada émit le Désaveu de 1875, supprimant ainsi la Loi relative aux terres de la C.-B., citant l’échec de la province à conclure des traités stipulant légalement la cession des terres par les Autochtones.</p>
+            </figcaption>
         </figure>
         <figure id="eight">
             <img src="/images/colonization/colonization7.png" alt="Panel 8 of 8" aria-describedby="panel-eight-transcript" />
+            <figcaption>
+                <p>En réponse à cela, la C.-B. menaça de se retirer du Canada…</p>
+                <p>En 1876, le Canada adopta la Loi sur les Indiens, qui avait pour effet d’étendre le contrôle gouvernemental sur tous les Autochtones, incluant ceux de la C.-B., " légalisant " ainsi le vol des territoires autochtones.</p>
+            </figcaption>
         </figure>
     </div>
     <button id="slider-transcript-toggle" class="transcript-toggle" aria-expanded="false"><span class="show-status">Afficher</span><span class="hide-status">Masquer</span> la transcription</button>
     <div class="transcript closed">
-        <div id="card-0">
+        <div id="card-0" tabindex="0">
             <h2>Panneau 1</h2>
-            <p>Cette bande dessinée est tirée de « L’album de 500 ans de résistance autochtone » (The 500 Years of Indigenous Resistance Comic Book) créé par l’artiste et historien Kwakwaka’wakw Gord Hill.</p>
+            <p>Cette bande dessinée est tirée de «&nbsp;L’album de 500 ans de résistance autochtone&nbsp;» (The 500 Years of Indigenous Resistance Comic Book) créé par l’artiste et historien Kwakwaka’wakw Gord Hill.</p>
             <p><strong>AVERTISSEMENT: </strong>Certaines images sont de nature très crue et peuvent être troublantes.</p>
         </div>
-        <div id="card-1">
+        <div id="card-1" tabindex="0">
             <h2>Panneau 2</h2>
-            <p>« Comment les Européens ont-ils colonisé la côte nord-ouest du Pacifique? » (Le titre est en blanc sur fond noir avec une police de caractères rappelant l’écriture à la craie. Toutes les images suivantes sont dessinées à la main et sont en noir et blanc.)</p>
+            <p>«&nbsp;Comment les Européens ont-ils colonisé la côte nord-ouest du Pacifique?&nbsp;» (Le titre est en blanc sur fond noir avec une police de caractères rappelant l’écriture à la craie. Toutes les images suivantes sont dessinées à la main et sont en noir et blanc.)</p>
         </div>
-        <div id="card-2">
+        <div id="card-2" tabindex="0">
             <h2>Panneau 3</h2>
-            <p id="panel-two-transcript">« Dans les années 1820, la Compagnie de la Baie d’Hudson constituait le principal pouvoir colonial de la région, même si le commerce des fourrures était en déclin. La compagnie détenait de nombreux forts et de nombreux bateaux. En 1828, un village Klallam, dans l’État de Washington, fut détruit par une canonnière de la Compagnie. » (Un dessin d’hommes sur une canonnière chargeant des canons et tirant sur un village côtier.)</p>
+            <p id="panel-two-transcript">«&nbsp;Dans les années 1820, la Compagnie de la Baie d’Hudson constituait le principal pouvoir colonial de la région, même si le commerce des fourrures était en déclin. La compagnie détenait de nombreux forts et de nombreux bateaux. En 1828, un village Klallam, dans l’État de Washington, fut détruit par une canonnière de la Compagnie.&nbsp;» (Un dessin d’hommes sur une canonnière chargeant des canons et tirant sur un village côtier.)</p>
         </div>
-        <div id="card-3">
+        <div id="card-3" tabindex="0">
             <h2>Panneau 4</h2>
-            <p id="panel-three-transcript">« En 1849, l’île de Vancouver fut déclarée colonie officielle avec la CBH agissant à titre de gouvernement. » (Un dessin d’un fort avec un drapeau britannique.)</p>
+            <p id="panel-three-transcript">«&nbsp;En 1849, l’île de Vancouver fut déclarée colonie officielle avec la CBH agissant à titre de gouvernement.&nbsp;» (Un dessin d’un fort avec un drapeau britannique.)</p>
         </div>
-        <div id="card-4">
+        <div id="card-4" tabindex="0">
             <h2>Panneau 5</h2>
-            <p id="panel-four-transcript">« Au même moment, l’établissement de colons et la vente de terres étaient devenus des sujets cruciaux pour le gouvernement… Pendant 30 ans, les canonnières de la Marine royale ont été utilisées pour servir le colonialisme britannique et contrôler les tribus côtières. » (Un dessin de trois hommes pointant une carte intitulée « Colonie de l’île de Vancouver ». Un des hommes dit : « Les Kwakiutl sont toujours des chasseurs de têtes? », ce à quoi répond un autre homme : « Oui monsieur… ») </p>
+            <p id="panel-four-transcript">«&nbsp;Au même moment, l’établissement de colons et la vente de terres étaient devenus des sujets cruciaux pour le gouvernement… Pendant 30 ans, les canonnières de la Marine royale ont été utilisées pour servir le colonialisme britannique et contrôler les tribus côtières.&nbsp;» (Un dessin de trois hommes pointant une carte intitulée «&nbsp;Colonie de l’île de Vancouver&nbsp;». Un des hommes dit : «&nbsp;Les Kwakiutl sont toujours des chasseurs de têtes?&nbsp;», ce à quoi répond un autre homme : «&nbsp;Oui monsieur…&nbsp;») </p>
         </div>
-        <div id="card-5">
+        <div id="card-5" tabindex="0">
             <h2>Panneau 6</h2>
-            <p id="panel-five-transcript">« Les canonnières servaient de forces militaire et policière… Elles étaient lourdement armées avec 50 canons et roquettes ainsi que des soldats de la Marine Royale. » (Un dessin de canons enlignés d’un côté d’une canonnière et d’un soldat de la Marine en uniforme qui regarde vers la rive, un mousquet entre les mains.)</p>
+            <p id="panel-five-transcript">«&nbsp;Les canonnières servaient de forces militaire et policière… Elles étaient lourdement armées avec 50 canons et roquettes ainsi que des soldats de la Marine Royale.&nbsp;» (Un dessin de canons enlignés d’un côté d’une canonnière et d’un soldat de la Marine en uniforme qui regarde vers la rive, un mousquet entre les mains.)</p>
         </div>
-        <div id="card-6">
+        <div id="card-6" tabindex="0">
             <h2>Panneau 7</h2>
-            <p id="panel-six-transcript">« En 1862, une épidémie de variole se déclara à Victoria. Les autorités coloniales ont forcé une centaine d’Autochtones à s’en aller. Infectés, ils sont retournés dans leurs villages et ont répandu la maladie. On estime qu’une personne sur trois est morte en 2 ans. L’épidémie s’est déclarée avec l’arrivée d’un plus grand nombre de colons européens. » (Un dessin d’une femme couverte de variole morte sur la plage. Un corbeau picosse son visage. Derrière il y a une maison longue et deux autres morts sur la plage.)</p>
+            <p id="panel-six-transcript">«&nbsp;En 1862, une épidémie de variole se déclara à Victoria. Les autorités coloniales ont forcé une centaine d’Autochtones à s’en aller. Infectés, ils sont retournés dans leurs villages et ont répandu la maladie. On estime qu’une personne sur trois est morte en 2 ans. L’épidémie s’est déclarée avec l’arrivée d’un plus grand nombre de colons européens.&nbsp;» (Un dessin d’une femme couverte de variole morte sur la plage. Un corbeau picosse son visage. Derrière il y a une maison longue et deux autres morts sur la plage.)</p>
         </div>
-        <div id="card-7">
+        <div id="card-7" tabindex="0">
             <h2>Panneau 8</h2>
-            <p id="panel-seven-transcript">« En 1871, la C.-B. est entrée dans le Canada. En 1874, la Loi relative aux terres de la C.-B. fut adoptée pour ouvrir le territoire à la colonisation. Le Canada émit le Désaveu de 1875, supprimant ainsi la Loi relative aux terres de la C.-B., citant l’échec de la province à conclure des traités stipulant légalement la cession des terres par les Autochtones. » (Une main tient un papier intitulé « Devoir de désaveu ».)</p>
+            <p id="panel-seven-transcript">«&nbsp;En 1871, la C.-B. est entrée dans le Canada. En 1874, la Loi relative aux terres de la C.-B. fut adoptée pour ouvrir le territoire à la colonisation. Le Canada émit le Désaveu de 1875, supprimant ainsi la Loi relative aux terres de la C.-B., citant l’échec de la province à conclure des traités stipulant légalement la cession des terres par les Autochtones.&nbsp;» (Une main tient un papier intitulé «&nbsp;Devoir de désaveu&nbsp;».)</p>
         </div>
-        <div id="card-8">
+        <div id="card-8" tabindex="0">
             <h2>Panneau 9</h2>
-            <p id="panel-eight-transcript">« En réponse à cela, la C.-B. menaça de se retirer du Canada… En 1876, le Canada adopta la Loi sur les Indiens, qui avait pour effet d’étendre le contrôle gouvernemental sur tous les Autochtones, incluant ceux de la C.-B., " légalisant " ainsi le vol des territoires autochtones ». (Une main tient un papier intitulé « Loi sur les Indiens de 1876 ». À côté, il y a d’autres papiers intitulés « Conseils de bande », « Réserves » et « Statut ».)</p>
+            <p id="panel-eight-transcript">«&nbsp;En réponse à cela, la C.-B. menaça de se retirer du Canada… En 1876, le Canada adopta la Loi sur les Indiens, qui avait pour effet d’étendre le contrôle gouvernemental sur tous les Autochtones, incluant ceux de la C.-B., " légalisant " ainsi le vol des territoires autochtones&nbsp;». (Une main tient un papier intitulé «&nbsp;Loi sur les Indiens de 1876&nbsp;». À côté, il y a d’autres papiers intitulés «&nbsp;Conseils de bande&nbsp;», «&nbsp;Réserves&nbsp;» et «&nbsp;Statut&nbsp;».)</p>
         </div>
     </div>
 </section>

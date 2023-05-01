ef361ac7b4187f2a5d83b2d780172ad7a1c665d1
Graham Douglas <graham.douglas@ubc.ca>
2023-05-01 14:36:24 -0700

    c507-gallery text alignment

--- en/encounters/gallery.php
+++ en/encounters/gallery.php
@@ -7,7 +7,10 @@
         <a href="/images/encounters/sea-otter-pelts.jpg" class="lightbox">
             <figure>
                 <img src="/images/encounters/sea-otter-pelts-thumb.jpg" alt="Two black and white photos. In one, a man wearing a bowler hat poses against a shiplap wall with three oval pelts. In the other a man stands beside one long pelt and a small baby pelt. In pen the first image has 'Sea Otter Skin. 1892 Ounakaska' written. The other has 'Sea Otter Skin Ounalaska.'">
-                <figcaption><p>Sea otter skins, ca.1892. Archival Photographer Stefan Claesson. Unalaska, Alaska, Gulf of Maine Cod Project, NOAA National Marine Sanctuaries; Courtesy of National Archives. These photos show how large and beautiful sea otter pelts are. They have a soft, thick coat of fur to keep them warm in the Pacific Ocean. Indigenous peoples traded the pelts with overseas fur traders starting in the 1700s. Sailors could sell the pelts in China for a small fortune. Within 200 years, sea otters were almost hunted to extinction on the coast.</p></figcaption>
+                <figcaption>
+<p>Sea otter skins, ca.1892. Archival Photographer Stefan Claesson. Unalaska, Alaska, Gulf of Maine Cod Project, NOAA National Marine Sanctuaries; Courtesy of National Archives.</p>
+
+<p>These photos show how large and beautiful sea otter pelts are. They have a soft, thick coat of fur to keep them warm in the Pacific Ocean. Indigenous peoples traded the pelts with overseas fur traders starting in the 1700s. Sailors could sell the pelts in China for a small fortune. Within 200 years, sea otters were almost hunted to extinction on the coast.</p></figcaption>
             </figure>
             
         </a>
@@ -16,7 +19,10 @@
         <a href="/images/encounters/nuu-chah-nulth-fish-hook.jpg" class="lightbox" >
             <figure>
                 <img src="/images/encounters/nuu-chah-nulth-fish-hook-thumb.jpg" alt="A thin peice of wood curves into a 'U' shape with a curve on one end and a sharp tusk pointed inwards on the other. The tusk is attached with leather and twine. The other end has leather wound around it for holding a line.">
-                <figcaption><p>Halibut fish hook, ca. 1790s. Courtesy of The British Museum Catalogue Number: Am, VAN.131.b. Nuu- chah-nulth designed this hook to catch halibut, a large flatfish that swims along the ocean floor. The hook is bent wood. It is 12.5cm long and 12.5cm wide. A piece of bait would be tied onto the bone barb. Then the hook would be lowered down into the water on a rope. Captain Vancouver&#39;s crew were often hungry because they didn&#39;t know how to hunt and fish on the Pacific Northwest Coast. They traded for Indigenous fishing gear, but also relied on Indigenous communities for food and fresh water. This fish hook was collected by the surgeon&#39;s mate on the HMS Discovery, George Goodman Hewett, from the Mowachaht/Muchalaht First Nation at Yuquot.</p></figcaption>
+                <figcaption>
+<p>Halibut fish hook, ca. 1790s. Courtesy of The British Museum Catalogue Number: Am, VAN.131.b.</p>
+
+<p>Nuu- chah-nulth designed this hook to catch halibut, a large flatfish that swims along the ocean floor. The hook is bent wood. It is 12.5cm long and 12.5cm wide. A piece of bait would be tied onto the bone barb. Then the hook would be lowered down into the water on a rope. Captain Vancouver&#39;s crew were often hungry because they didn&#39;t know how to hunt and fish on the Pacific Northwest Coast. They traded for Indigenous fishing gear, but also relied on Indigenous communities for food and fresh water. This fish hook was collected by the surgeon&#39;s mate on the HMS Discovery, George Goodman Hewett, from the Mowachaht/Muchalaht First Nation at Yuquot.</p></figcaption>
             </figure>
         </a>
     </li>
@@ -24,7 +30,10 @@
         <a href="/images/encounters/cat-o-nine-tails.jpg" class="lightbox" data-caption="Cat-o'-nine tails whip, ca. 1700-1850\nCourtesy of Wellcome Collection A cat-o'-nine tails was a whip used by the British Royal Navy to punish sailors. It consists of nine pieces of cord each tied with a series of knots. Sailors could receive up to 24 whip lashes across their backs at a time. Like other Royal Navy commanders, Captain Vancouver regularly punished his crew this way.">
             <figure>
                 <img src="/images/encounters/cat-o-nine-tails-thumb.jpg" alt="A wooden rod with a short leather strap bound to it's end. The leather strap has nine cords tied to it's end. The cords have multiple knots tied to them. The bottom of the rod shows heavy signs of wear from being handled.">
-                <figcaption id="cat-o-nine"><p>Cat-o&#39;-nine tails whip, ca. 1700-1850. Courtesy of Wellcome Collection. A cat-o&#39;-nine tails was a whip used by the British Royal Navy to punish sailors. It consists of nine pieces of cord each tied with a series of knots. Sailors could receive up to 24 whip lashes at a time across their bare backs. Like other Royal Navy commanders, Captain Vancouver regularly punished his crew this way.</p></figcaption>
+                <figcaption id="cat-o-nine">
+<p>Cat-o&#39;-nine tails whip, ca. 1700-1850. Courtesy of Wellcome Collection.</p>
+
+<p>A cat-o&#39;-nine tails was a whip used by the British Royal Navy to punish sailors. It consists of nine pieces of cord each tied with a series of knots. Sailors could receive up to 24 whip lashes at a time across their bare backs. Like other Royal Navy commanders, Captain Vancouver regularly punished his crew this way.</p></figcaption>
             </figure>
         </a>
     </li>
--- fr/navigation/gallery.php
+++ fr/navigation/gallery.php
@@ -9,11 +9,9 @@
                     <img src="/images/navigation/sextant-thumb.jpg"
                         alt="Un sextant en laiton. Il est triangulaire avec un arc sur le fond.">
                     <figcaption>
-                        <p>Sextant de cuivre à cinq oculaires, v. 1790. Fabriqué par Jesse Ramsden, utilisé par Joseph
-                            Baker, lieutenant de Vancouver, et prêté au Vancouver Maritime Museum, courtoisie de Richard
-                            Baker. Le sextant est un instrument de navigation à double réflexion avec un arc gradué de
-                            60° et un mécanisme de visée, utilisé pour mesurer les distances angulaires entre des objets
-                            et surtout pour calculer des altitudes en navigation.</p>
+<p>Sextant de cuivre à cinq oculaires, v. 1790.</p>
+
+<p>Fabriqué par Jesse Ramsden, utilisé par Joseph Baker, lieutenant de Vancouver, et prêté au Vancouver Maritime Museum, courtoisie de Richard Baker. Le sextant est un instrument de navigation à double réflexion avec un arc gradué de 60° et un mécanisme de visée, utilisé pour mesurer les distances angulaires entre des objets et surtout pour calculer des altitudes en navigation.</p>
                     </figcaption>
                 </figure>
             </a>
@@ -24,11 +22,9 @@
                     <img src="/images/navigation/telescope-thumb.jpg"
                         alt="Un télescope avec une pointe en laiton qui tient la lentille et l'oculaire. Le corps est en cuir brillant et patiné.">
                     <figcaption>
-                        <p>Téléscope en deux morceaux, recouvert de cuir, v. 1790. Fabriqué par G. ou P. Dolland, avec
-                            inscription «&nbsp;Capt. John. Baker Royal Navy&nbsp;», prêté au Vancouver Maritime Museum,
-                            courtoisie de Richard Baker. Joseph Baker était le lieutenant de Vancouver. Un télescope est
-                            un instrument optique qui magnifie les objets éloignés en disposant des lentilles ou des
-                            miroirs incurvés de façon à recueillir et focaliser la lumière.</p>
+<p>Téléscope en deux morceaux, recouvert de cuir, v. 1790.</p>
+
+<p>Fabriqué par G. ou P. Dolland, avec inscription «&nbsp;Capt. John. Baker Royal Navy&nbsp;», prêté au Vancouver Maritime Museum, courtoisie de Richard Baker. Joseph Baker était le lieutenant de Vancouver. Un télescope est un instrument optique qui magnifie les objets éloignés en disposant des lentilles ou des miroirs incurvés de façon à recueillir et focaliser la lumière.</p>
                     </figcaption>
                 </figure>
             </a>
@@ -39,14 +35,9 @@
                     <img src="/images/navigation/chronometer-thumb.jpg"
                         alt="Une boîte en bois s'est ouverte, avec à l'intérieur un mécanisme ressemblant à une horloge.">
                     <figcaption>
-                        <p>Chronomètre “Arnold 176”, v. 1791. Fabriqué par John Arnold et utilisé par George Vancouver
-                            pour son expédition sur la côte nord-ouest du Pacifique. Propriété du Vancouver Maritime
-                            Museum. Un chronomètre marin est une montre suffisamment précise et exacte pour être
-                            utilisée comme étalon de temps portatif, ce qui signifie qu’il peut être utilisé pour
-                            déterminer la longitude en mesurant avec précision l’heure d’un emplacement fixe connu. Au
-                            moment de son invention, au XVIIIe siècle, il s’agissait d’une réalisation technique
-                            majeure, car une connaissance précise de l’heure pendant les voyages en mer est nécessaire à
-                            la navigation dépourvue d’appareils électroniques ou de communication.</p>
+<p>Chronomètre “Arnold 176”, v. 1791.</p>
+
+<p>Fabriqué par John Arnold et utilisé par George Vancouver pour son expédition sur la côte nord-ouest du Pacifique. Propriété du Vancouver Maritime Museum. Un chronomètre marin est une montre suffisamment précise et exacte pour être utilisée comme étalon de temps portatif, ce qui signifie qu’il peut être utilisé pour déterminer la longitude en mesurant avec précision l’heure d’un emplacement fixe connu. Au moment de son invention, au XVIIIe siècle, il s’agissait d’une réalisation technique majeure, car une connaissance précise de l’heure pendant les voyages en mer est nécessaire à la navigation dépourvue d’appareils électroniques ou de communication.</p>
                     </figcaption>
                 </figure>
             </a>

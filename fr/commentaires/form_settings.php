2bea3aaeed4f1151a7f11feb878ccca3f594cd3e
Graham Douglas <graham.douglas@ubc.ca>
2023-05-01 11:15:38 -0700

    424-feddback-form-language-confirmation

--- fr/commentaires/form_settings.php
+++ fr/commentaires/form_settings.php
@@ -4,4 +4,4 @@
 $email_to = "grm.dgs@gmail.com";
 $system_address = "donotreply@changingperspectives.grmdgs.com";
 $email_subject = "Nouvelles perspectives"; // email subject line
-$thankyou = "thankyou.php"; // thank you page
+$thankyou = "merci_thank_you.php"; // thank you page

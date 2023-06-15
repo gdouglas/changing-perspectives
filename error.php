2aa6a9613c34aa6758f9c0167615763a5077db0f
Graham Douglas <graham.douglas@ubc.ca>
2023-06-15 12:09:16 -0700

    feat/php-error-page

---
+++ error.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Error Page</title>
+</head>
+<body>
+    <?php
+    // Determine the language based on user preferences or other criteria
+    $preferredLanguage = 'en'; // Set default language to English
+
+    // Check if the user's preferred language is French
+    if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) && strpos($_SERVER['HTTP_ACCEPT_LANGUAGE'], 'fr') !== false) {
+        $preferredLanguage = 'fr';
+    }
+
+    // Error messages in English and French
+    $errorMessages = [
+        'en' => [
+            '404' => 'Page not found.',
+            '500' => 'Internal Server Error.',
+        ],
+        'fr' => [
+            '404' => 'Page non trouvée.',
+            '500' => 'Erreur interne du serveur.',
+        ],
+    ];
+
+    // Get the HTTP error code
+    $errorCode = http_response_code();
+
+    // Get the appropriate error message based on the language and error code
+    $errorMessage = $errorMessages[$preferredLanguage][$errorCode];
+
+    // Display the error message
+    echo "<h1>$errorMessage</h1>";
+    ?>
+</body>
+</html>

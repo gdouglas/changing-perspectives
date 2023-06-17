ec14adc136f3798daecbfbf495351310296173e8
Graham Douglas <graham.douglas@ubc.ca>
2023-06-17 13:47:59 -0700

    bugfix/language-detection not using url as primary

--- language-detect.php
+++ language-detect.php
@@ -1,48 +1,56 @@
 <?php
 session_start();
 
-function detectUrlLanguagePreference() {
+function detectUrlLanguagePreference()
+{
     // Check if '/en' is present in the current URL
     if (strpos($_SERVER['REQUEST_URI'], '/en') === 0) {
         return 'en'; // English
     }
-    
+
     // Check if '/fr' is present in the current URL
     if (strpos($_SERVER['REQUEST_URI'], '/fr') === 0) {
         return 'fr'; // French
     }
-    
-    // Default to null if no language preference is found in the URL
-    return null;
+    // URL does not have a language value
+    return getLanguageFromSession();
 }
 
-// Check if the language preference is set in the URL
-$language = detectUrlLanguagePreference();
+function getLanguageFromSession()
+{
+    if (isset($_SESSION['lang'])) {
+        return $_SESSION['lang'];
+    } else {
+        return getBrowserLanguage();
+    }
+}
 
-// If the language preference is not set in the URL, check the session variable
-if ($language === null && isset($_SESSION['lang'])) {
-    $language = $_SESSION['lang'];
-} else {
+function getBrowserLanguage()
+{
     // Check if the user has a language preference in their browser
     if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
         $acceptedLanguages = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
-        
+
         foreach ($acceptedLanguages as $language) {
             if (strpos($language, 'fr') !== false) {
-                $language = 'fr'; // French
-                break;
+                return 'fr'; // French
             } elseif (strpos($language, 'en') !== false) {
-                $language = 'en'; // English
-                break;
+                return 'en'; // English
             }
         }
     }
-    
-    // If no language preference is found, default to English
-    if ($language === null) {
-        $language = 'en'; // English
-    }
-    
-    // Update the session variable with the detected language preference
-    $_SESSION['lang'] = $language;
+
+    // Fallback to a default language
+    return 'en';
 }
+
+function setLanguage()
+{
+    return detectUrlLanguagePreference();
+}
+
+// Check if the language preference is set in the URL
+$language = setLanguage();
+
+// Update the session variable with the detected language preference
+$_SESSION['lang'] = $language;
